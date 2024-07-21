f4aef1e0c4e020c7332d3d2ba106d7c94fa1dc3c
selda kabir <seldaakabirnejat@gmail.com>
2024-07-21 12:45:19 +0300

    question

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,6 +2,8 @@
 export default function Question({ questions,index }) {
     
     return <div>
-       
+        <p>{questions[index].question}</p>
+        {questions.map((question,index) =>
+            <button>{question.options[index] }</button>)}
   </div>
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,10 +1,9 @@
 
-
 export default function StartScreen ({index,questions,dispatch}) {
     return <div className="start">
         <h2>Wlcome to the ract quiz!</h2>
         <h3 >{questions.length} questions to test your react mastery</h3>
-        <button className="btn " onClick={()=>dispatch({type:'showQuestions'})} >Let's start</button>
+        <button className="btn" onClick={()=>dispatch({type:'showQuestions'})} >Let's start</button>
     </div>
   
 }
